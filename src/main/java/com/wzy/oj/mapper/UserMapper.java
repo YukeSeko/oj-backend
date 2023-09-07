d833b5395cb729bcdb7a8bf71c7f87eba28a88a4
王灼宇 <1286694258@qq.com>
2023-09-07 17:25:26 +0800

    修改配置包名

---
+++ src/main/java/com/wzy/oj/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.wzy.oj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wzy.oj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
