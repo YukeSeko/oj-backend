3e2f8a1e44e53a3defeba7af21fcb4b220e0b754
王灼宇 <1286694258@qq.com>
2023-09-20 17:29:41 +0800

    添加题目实体，及相关代码

---
+++ src/main/java/com/wzy/oj/mapper/QuestionMapper.java
@@ -0,0 +1,14 @@
+package com.wzy.oj.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wzy.oj.model.entity.Question;
+
+
+public interface QuestionMapper extends BaseMapper<Question> {
+
+}
+
+
+
+
---
+++ src/main/java/com/wzy/oj/mapper/QuestionSubmitMapper.java
@@ -0,0 +1,14 @@
+package com.wzy.oj.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wzy.oj.model.entity.QuestionSubmit;
+
+
+public interface QuestionSubmitMapper extends BaseMapper<QuestionSubmit> {
+
+}
+
+
+
+
